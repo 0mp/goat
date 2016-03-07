ea9b8b107f0e191b30bac791b7e83a41c431fdd7
Mateusz Piotrowski <mpp302@gmail.com>
2016-03-07 03:35:26 +0100

    Add installation script.

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SHELLRC_FILE="${HOME}/.bashrc"
+INSTALLATION_DIR='/usr/local/lib/goat'
+CONFIGURATION_DIR="${HOME}/.goat"
+
+echo "Installing goat to ${INSTALLATION_DIR} ..."
+if [ ! -d "$INSTALLATION_DIR" ]; then
+    mkdir $INSTALLATION_DIR
+fi
+cp goat.rb $INSTALLATION_DIR/goat.rb
+
+echo "Adding configuration files ..."
+if [ ! -d "$CONFIGURATION_DIR" ]; then
+    mkdir $CONFIGURATION_DIR
+fi
+
+
+echo "Adding alias to ${SHELLRC_FILE} ..."
+
+echo '
+# Added by goat.
+function GoatThere {
+    cd `ruby'" ${INSTALLATION_DIR}"'/goat.rb $1 $2 2>/dev/stderr`
+}
+alias goat=GoatThere' >> $SHELLRC_FILE
+
+echo Done!
