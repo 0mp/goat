a13162d1c57a7367350319c8dec02a09f376aa8c
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-09-09 21:39:50 +0200

    Bump the copyright year

--- libgoat.sh
+++ libgoat.sh
@@ -2,7 +2,9 @@
 #
 # ---
 #
-# Copyright (c) 2017 Mateusz Piotrowski <0mp@FreeBSD.org>
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright 2017-2018 Mateusz Piotrowski <0mp@FreeBSD.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
